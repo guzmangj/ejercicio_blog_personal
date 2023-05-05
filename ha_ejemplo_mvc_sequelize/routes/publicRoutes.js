8911c03f331aa12dbacafdb4bb319cfa13719a44
Guillermo Sanchez <guillesan3@gmail.com>
2023-05-05 15:07:30 -0300

    views y conexiones

--- ha_ejemplo_mvc_sequelize/routes/publicRoutes.js
+++ ha_ejemplo_mvc_sequelize/routes/publicRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const pagesController = require("../controllers/pagesController");
 
-// Rutas relacionadas a la parte pública del sitio web:
-// ...
+router.get("/", pagesController.showHome);
 
 module.exports = router;
