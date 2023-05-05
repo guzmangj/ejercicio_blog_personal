87bae3480d337fa19ed55abc644b015ad5a7be1b
Juan Andrés Chítaro <juanchitaro@gmail.com>
2023-05-05 15:09:17 -0300

    Cambios juan

--- ha_ejemplo_mvc_sequelize/routes/index.js
+++ ha_ejemplo_mvc_sequelize/routes/index.js
@@ -21,13 +21,6 @@ const publicRoutes = require("./publicRoutes");
 const privateRoutes = require("./privateRoutes");
 
 module.exports = (app) => {
-  /**
-   * Notar que si el sitio está en español, tiene sentido que las URLs que se
-   * ven en la barra de direcciiones del navegador también lo estén. No así los
-   * nombres de variables, funciones, etc, que siempre se recomienda que estén
-   * en inglés.
-   */
-
   app.use("/usuarios", userRoutes);
   app.use("/articulos", articleRoutes);
   app.use("/comentarios", commentRoutes);
