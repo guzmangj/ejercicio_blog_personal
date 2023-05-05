bb32a17a486b91c9e6a70d0e116cf4967a30a191
facugroso <110434971+facugroso@users.noreply.github.com>
2023-05-05 15:53:24 -0300

    linqueando rutas

--- ha_ejemplo_mvc_sequelize/routes/privateRoutes.js
+++ ha_ejemplo_mvc_sequelize/routes/privateRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const pagesController = require("../controllers/pagesController");
 
 // Rutas relacionadas al panel de control (Admin):
+router.get("/", pagesController.showPanel);
 // ...
 
 module.exports = router;
