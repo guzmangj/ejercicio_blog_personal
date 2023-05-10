ba4aa72cbf23d9644a8f2e21cb3315aa8987e289
guzmangj <guzmangj96@gmail.com>
2023-05-10 12:52:54 -0300

    passport.authenticate is not a function

---
+++ ha_ejemplo_mvc_sequelize/config/passport.js
@@ -0,0 +1,42 @@
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+
+function passportConfig() {
+  passport.use(
+    new LocalStrategy({ usernameField: "email" }, async function (email, password, done) {
+      try {
+        const user = await User.findOne({ where: { email } });
+        if (!user) {
+          console.log("Nombre de usuario no existe.");
+          return done(null, false, { message: "Credenciales incorrectas." });
+        }
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+          console.log("La contraseña es inválida.");
+          return done(null, false, { message: "Credenciales incorrectas." });
+        }
+        console.log("Credenciales verificadas correctamente");
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    }),
+  );
+
+  passport.serializeUser((user, done) => {
+    return done(null, user.id);
+  });
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findByPk(id);
+      return done(null, user); // Usuario queda disponible en req.user.
+    } catch (err) {
+      return done(err);
+    }
+  });
+}
+
+module.exports = {
+  passportConfig,
+};
