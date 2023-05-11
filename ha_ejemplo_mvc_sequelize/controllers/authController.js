7bde979d8f58cfd061e17ad6505854743d7c2663
estebanCastaneira <estebanc_b@hotmail.com>
2023-05-11 17:03:36 -0300

    Se retiran y corrigen rutas inexistentes

--- ha_ejemplo_mvc_sequelize/controllers/authController.js
+++ ha_ejemplo_mvc_sequelize/controllers/authController.js
@@ -3,7 +3,7 @@ const { passport } = require("../config/passport");
 async function login(req, res, next) {
   passport.authenticate("local", {
     successRedirect: "/panel",
-    failureRedirect: "/usuarios/login",
+    failureRedirect: "/login",
     failureFlash: {
       type: "failureFlash",
       message: "todo mal, no te tengo en la DB",
--- ha_ejemplo_mvc_sequelize/routes/userRoutes.js
+++ ha_ejemplo_mvc_sequelize/routes/userRoutes.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-const ensureAuthenticated = require("../middleware/ensureAuthenticated");
-const authController = require("../controllers/authController");
 // Rutas relacionadas a los usuarios:
 // ...
 
-router.get("/login", authController.login);
+
 router.get("/registro", userController.create);
 router.post("/registro", userController.store);
 router.get("/:id", userController.show);
 router.get("/:id/editar", userController.edit);
 router.patch("/:id", userController.update);
 router.delete("/:id", userController.destroy);
-router.get("/welcome", userController.showWelcome);
+
 
 module.exports = router;
