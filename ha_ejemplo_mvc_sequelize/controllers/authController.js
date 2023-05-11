023d4e6aaa5f5f2a32c741730472c5a08a126a35
estebanCastaneira <estebanc_b@hotmail.com>
2023-05-11 11:32:13 -0300

    se borra lo relacionado a welcome, empezamos con flash

--- ha_ejemplo_mvc_sequelize/controllers/authController.js
+++ ha_ejemplo_mvc_sequelize/controllers/authController.js
@@ -4,6 +4,10 @@ async function login(req, res, next) {
   passport.authenticate("local", {
     successRedirect: "/panel",
     failureRedirect: "/usuarios/login",
+    failureFlash: {
+      type: "failureFlash",
+      message: "todo mal, no te tengo en la DB"
+    }
   })(req, res);
 }
 
--- ha_ejemplo_mvc_sequelize/routes/privateRoutes.js
+++ ha_ejemplo_mvc_sequelize/routes/privateRoutes.js
@@ -5,11 +5,6 @@ const pagesController = require("../controllers/pagesController");
 const authController = require("../controllers/authController");
 
 router.get("/", ensureAuthenticated, pagesController.showPanel);
-
-router.get("/welcome", ensureAuthenticated, function (req, res) {
-  res.send(`Te damos la bienvenida, ${req.user.firstname}!!!`);
-});
-
 router.post("/login", authController.login);
 
 
